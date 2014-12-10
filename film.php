053a41f419a8a1b925c0d3525e2b3c5a928610df
Lendina <lh6g13@soton.ac.uk>
2014-12-10 22:54:10 +0000

    film page
    connected the film database to the page, added a function to showcase
    all the films in the database

--- film.php
+++ film.php
@@ -2,8 +2,26 @@
     require_once('includes/predispatch.php');
 require_once('includes/header.php');
 require('includes/db.php');
+	require_once('classes/film.class.php');
+	$films = $db->query("SELECT * FROM `film`");
 
 ?>
+<h1>This is a list of all the films available!</h1>
+<div id="film-list" class="row">
+<div class="list-group row text-center" >
+	<?php
+		while($film = $films->fetch_object('Film')): 
+			?>
+		<div class="filmBox col-md-3">
+		<a href="show-film.php?id=<?=$film->id?>" class="list-group-item">
+		<?php echo "<img style='height:190px; width:120px' src='http://comp2203.ecs.soton.ac.uk/coursework/1415/assets/posters/" .$film -> id. "_small.jpg'>"  ?>	
+		<p class="list-group-item-text overflow" ><br><?=$film->name?></p><br>
+		</a>
+		</div>	
+		<?php endwhile; ?>
+
+		</div>
+		</div>
 
 
 
