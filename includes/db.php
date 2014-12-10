4b839244d5ea0b5e375bcd61864c41a043625680
Lendina <lh6g13@soton.ac.uk>
2014-12-10 22:28:22 +0000

    Included the database

---
+++ includes/db.php
@@ -0,0 +1,14 @@
+<?php
+
+// database credentials
+$dbHost		 = 'comp2203.ecs.soton.ac.uk';
+$dbUsername	 = 'comp2203-cw-1415';
+$dbPassword	 = '8VMMx2m80YsN4t3T2Oc28hO38F49NC';
+$dbSchema	 = 'comp2203-cw-1415';
+
+$db = new mysqli($dbHost, $dbUsername, $dbPassword, $dbSchema);
+
+// check if the database connection failed
+if($db->connect_errno){
+	echo "Failed to connect to MySQL: " . $db->connect_error;
+}
