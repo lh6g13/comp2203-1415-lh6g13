ed74bb4f74785e5eca2542c097050155620dcc55
Lendina <lh6g13@soton.ac.uk>
2014-12-10 22:35:58 +0000

    Header
    Completed the header

---
+++ includes/header.php
@@ -0,0 +1,19 @@
+<?php require('includes/head.php'); ?>
+
+<div id="header">
+	<div id="top">
+
+	<div class="row">
+		<div class="col-md-3 pull-left"><img src="img/red.png" style="width:128px;height:128px"></div>
+		<div class="col-md-6"s><h1>Red Carpet Films</h1></div>
+		<div class="col-md-3 pull-right"><img src="img/red.png" style="width:128px;height:128px"></div>
+	</div>
+	
+
+	<h2 id="motd">Experience the world cinema in your own home!</h2>
+	
+
+	<?php require('includes/navigation.php'); ?>
+	
+	</div>
+</div>
