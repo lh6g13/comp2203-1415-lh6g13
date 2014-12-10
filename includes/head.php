792d0c13927e54e675813eb90eba689a74592ff1
Lendina <lh6g13@soton.ac.uk>
2014-12-10 22:27:59 +0000

    Finished the head

---
+++ includes/head.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Red Carpet Films</title>
+		
+		<link href="css/bootstrap.min.css" type="text/css" rel="stylesheet" media="screen">
+		<link href="css/style.css" type="text/css" rel="stylesheet" media="screen">
+		<link href="css/jquery-ui.css" type= "text/css" rel="stylesheet" media="screen">
+
+		
+		<script src="//code.jquery.com/jquery.js" type="text/javascript"></script>
+		<script src="js/bootstrap.min.js" type="text/javascript"></script>
+		<script type="text/javascript" src="js/formscript.js"></script>
+		<script type="text/javascript" src="js/jquery-ui.js"></script>
+		<link href="css/carousel.css" type="text/css" rel="stylesheet" media="screen">
+		
+	</head>
+		
+	<body>
+	
+		<!-- container -->
+		<div class="container">
