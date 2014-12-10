6a968de4764394172f607055849ef06d2f1f3aaa
Lendina <lh6g13@soton.ac.uk>
2014-12-10 22:51:53 +0000

    Genres
    Connected the genres database and created a function so that all the
    genres would show in the website

--- genres.php
+++ genres.php
@@ -2,8 +2,23 @@
     require_once('includes/predispatch.php');
 require_once('includes/header.php');
 require('includes/db.php');
+require_once('classes/genre.class.php');
+
+
+$genres = $db->query("SELECT * FROM genre");
 
 ?>
+		<div id="genre-list" class="row">
+		<div class="list-group  row text-center" class="list-group col-md-6">
+			<?php
+			while($genre = $genres->fetch_object('Genre')): ?>
+				<a href="show-genre.php?id=<?=$genre->id?>" class="list-group-item">
+				<h4 class="list-group-item-heading"><?=$genre->name?></h4>	
+				</a>
+			<?php endwhile; ?>
+		</div>
+	</div>
+</div>
 
 
 
