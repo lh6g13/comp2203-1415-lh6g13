0463a5cf870d055f5326c6e580d72e1075bc114b
Lendina <lh6g13@soton.ac.uk>
2014-12-10 22:56:33 +0000

    Random
    Added the piece of code for the random film

---
+++ partials/random.php
@@ -0,0 +1,13 @@
+<div class="col-md-4"></div>
+<div class="col-md-4">
+				<a href="show-film.php?id=<?=$film->id?>" class="list-group-item">
+				<p class="list-group-item-text"> Random Film of the Day: <?=$film->name?></p>
+				<?php echo "<img src='http://comp2203.ecs.soton.ac.uk/coursework/1415/assets/posters/" .$film -> id. "_medium.jpg'>" ?>
+					
+					
+					
+
+					
+				</a>
+				</div>
+				<div class="col-md-4"></div>
